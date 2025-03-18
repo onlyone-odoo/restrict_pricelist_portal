58306632cc252159397834bac521d4b84df2b1ca
onlyone-odoo <matiasb@onlyone.com.ar>
2025-03-18 00:46:14 -0300

    final4

--- controllers/website_sale.py
+++ controllers/website_sale.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
+from odoo.tools import _  # Añadimos esta importación
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
