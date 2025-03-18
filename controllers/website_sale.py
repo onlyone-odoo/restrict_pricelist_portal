9ecc169fab47d3a0391ed770954dcf2810827c1d
onlyone-odoo <matiasb@onlyone.com.ar>
2025-03-17 23:58:50 -0300

    [UPD]controller

--- controllers/website_sale.py
+++ controllers/website_sale.py
@@ -11,13 +11,12 @@ class WebsiteSaleRestrictedPricelists(WebsiteSale):
             WebsiteSaleRestrictedPricelists, self
         )._get_pricelist_available(show_visible=show_visible)
 
-        # Si el usuario es del portal, aplicar la restricción
-        if request.env.user.has_group("base.group_portal"):
-            partner = request.env.user.partner_id
-            allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
-            if allowed_pricelist_ids:
-                max_allowed_id = max(allowed_pricelist_ids)
-                pricelists = pricelists.filtered(lambda pl: pl.id <= max_allowed_id)
+        # Aplicar restricción si el contacto tiene allowed_pricelist_ids definido
+        partner = request.env.user.partner_id
+        allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
+        if allowed_pricelist_ids:
+            max_allowed_id = max(allowed_pricelist_ids)
+            pricelists = pricelists.filtered(lambda pl: pl.id <= max_allowed_id)
 
         return pricelists
 
@@ -54,14 +53,13 @@ class WebsiteSaleRestrictedPricelists(WebsiteSale):
             **post,
         )
 
-        # Si el usuario es del portal, actualizar las listas de precios en el qcontext
-        if request.env.user.has_group("base.group_portal"):
-            partner = request.env.user.partner_id
-            allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
-            if allowed_pricelist_ids:
-                max_allowed_id = max(allowed_pricelist_ids)
-                available_pricelists = self._get_pricelist_available(show_visible=True)
-                response.qcontext["website_sale_pricelists"] = available_pricelists
+        # Actualizar las listas de precios en el qcontext si hay restricción
+        partner = request.env.user.partner_id
+        allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
+        if allowed_pricelist_ids:
+            max_allowed_id = max(allowed_pricelist_ids)
+            available_pricelists = self._get_pricelist_available(show_visible=True)
+            response.qcontext["website_sale_pricelists"] = available_pricelists
 
         return response
 
@@ -73,15 +71,14 @@ class WebsiteSaleRestrictedPricelists(WebsiteSale):
         sitemap=False,
     )
     def pricelist_change(self, pricelist, **post):
-        # Si el usuario es del portal, validar que la lista seleccionada esté permitida
-        if request.env.user.has_group("base.group_portal"):
-            partner = request.env.user.partner_id
-            allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
-            if allowed_pricelist_ids:
-                max_allowed_id = max(allowed_pricelist_ids)
-                if pricelist.id > max_allowed_id:
-                    # Redirigir a la página de la tienda sin cambiar la lista
-                    return request.redirect(post.get("r", "/shop"))
+        # Validar que la lista seleccionada esté permitida si hay restricción
+        partner = request.env.user.partner_id
+        allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
+        if allowed_pricelist_ids:
+            max_allowed_id = max(allowed_pricelist_ids)
+            if pricelist.id > max_allowed_id:
+                # Redirigir a la página de la tienda sin cambiar la lista
+                return request.redirect(post.get("r", "/shop"))
 
         return super(WebsiteSaleRestrictedPricelists, self).pricelist_change(
             pricelist, **post
@@ -91,20 +88,19 @@ class WebsiteSaleRestrictedPricelists(WebsiteSale):
         ["/shop/pricelist"], type="http", auth="public", website=True, sitemap=False
     )
     def pricelist(self, promo, **post):
-        # Si el usuario es del portal, validar el código de promoción
-        if request.env.user.has_group("base.group_portal"):
-            partner = request.env.user.partner_id
-            allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
-            if allowed_pricelist_ids and promo:
-                max_allowed_id = max(allowed_pricelist_ids)
-                pricelist_sudo = (
-                    request.env["product.pricelist"]
-                    .sudo()
-                    .search([("code", "=", promo)], limit=1)
-                )
-                if pricelist_sudo and pricelist_sudo.id > max_allowed_id:
-                    redirect = post.get("r", "/shop/cart")
-                    return request.redirect(f"{redirect}?code_not_available=1")
+        # Validar el código de promoción si hay restricción
+        partner = request.env.user.partner_id
+        allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
+        if allowed_pricelist_ids and promo:
+            max_allowed_id = max(allowed_pricelist_ids)
+            pricelist_sudo = (
+                request.env["product.pricelist"]
+                .sudo()
+                .search([("code", "=", promo)], limit=1)
+            )
+            if pricelist_sudo and pricelist_sudo.id > max_allowed_id:
+                redirect = post.get("r", "/shop/cart")
+                return request.redirect(f"{redirect}?code_not_available=1")
 
         return super(WebsiteSaleRestrictedPricelists, self).pricelist(promo, **post)
 
@@ -113,20 +109,19 @@ class WebsiteSaleRestrictedPricelists(WebsiteSale):
             product, category, search, **kwargs
         )
 
-        # Si el usuario es del portal, asegurar que el pricelist esté permitido
-        if request.env.user.has_group("base.group_portal"):
-            partner = request.env.user.partner_id
-            allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
-            if allowed_pricelist_ids:
-                max_allowed_id = max(allowed_pricelist_ids)
-                current_pricelist = values["pricelist"]
-                available_pricelists = self._get_pricelist_available(show_visible=True)
-                if current_pricelist.id > max_allowed_id:
-                    values["pricelist"] = (
-                        available_pricelists[0]
-                        if available_pricelists
-                        else request.website.get_current_pricelist()
-                    )
-                values["website_sale_pricelists"] = available_pricelists
+        # Asegurar que el pricelist esté permitido si hay restricción
+        partner = request.env.user.partner_id
+        allowed_pricelist_ids = partner.allowed_pricelist_ids.ids
+        if allowed_pricelist_ids:
+            max_allowed_id = max(allowed_pricelist_ids)
+            current_pricelist = values["pricelist"]
+            available_pricelists = self._get_pricelist_available(show_visible=True)
+            if current_pricelist.id > max_allowed_id:
+                values["pricelist"] = (
+                    available_pricelists[0]
+                    if available_pricelists
+                    else request.website.get_current_pricelist()
+                )
+            values["website_sale_pricelists"] = available_pricelists
 
         return values
