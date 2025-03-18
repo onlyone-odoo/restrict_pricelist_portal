e5feab71b483e6c0c043b78e79a0d0ae4620753f
onlyone-odoo <matiasb@onlyone.com.ar>
2025-03-17 23:41:52 -0300

    init en controllers

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
     "website": "https://onlyone.odoo.com/",
     "license": "AGPL-3",
     "category": "Website",
-    "version": "17.0.1.0.0",
+    "version": "17.0.2.0.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
